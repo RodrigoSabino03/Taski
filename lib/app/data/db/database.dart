9dfde6193e41798ea1072cf95b11944e7e85868f
RodrigoSabino02 <rodrigosabinosdm@gmail.com>
2025-01-05 18:20:32 -0300

    create database

---
+++ lib/app/data/db/database.dart
@@ -0,0 +1,72 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'package:todolist/app/data/model/task_model.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper _instance = DatabaseHelper._internal();
+  factory DatabaseHelper() => _instance;
+  static Database? _database;
+
+  DatabaseHelper._internal();
+
+  Future<Database> get database async {
+    _database ??= await _initDb();
+    return _database!;
+  }
+
+  Future<Database> _initDb() async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, 'tasks.db');
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: (db, version) async {
+        await db.execute('''
+          CREATE TABLE tasks(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            isDone INTEGER
+          )
+        ''');
+      },
+    );
+  }
+
+  Future<int> insertTask(Task task) async {
+    final db = await database;
+    return await db.insert('tasks', task.toMap());
+  }
+
+  Future<List<Task>> getTasks() async {
+    final db = await database;
+    final List<Map<String, dynamic>> maps = await db.query('tasks');
+    return List.generate(maps.length, (i) {
+      return Task.fromMap(maps[i]);
+    });
+  }
+
+  Future<int> updateTask(Task task) async {
+    final db = await database;
+    return await db.update(
+      'tasks',
+      task.toMap(),
+      where: 'id = ?',
+      whereArgs: [task.id],
+    );
+  }
+
+  Future<int> deleteTask(int id) async {
+    final db = await database;
+    return await db.delete(
+      'tasks',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+
+  Future<int> deleteAllTasks() async {
+    final db = await database;
+    return await db.delete('tasks');
+  }
+}
